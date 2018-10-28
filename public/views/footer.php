4d6827238d683e908900a1050ad5400a9d3912a0
Jessie Edmisten <jaedmisten@gmail.com>
2018-10-27 20:32:59 -0400

    Updates to the footer.

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    &copy;MovieSite Send complaints to <a href="mailto:idont@care.com">idont@care.com</a>
+    <p class="footer-text">&copy;MovieSite Send complaints to <a href="mailto:idont@care.com">idont@care.com</a></p>
 </div>
 <script>
     console.log('test');
