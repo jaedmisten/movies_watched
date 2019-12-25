4cd9d37655e0e4dda5dd007adbeb73432b181b48
Jessie Edmisten <jaedmisten@gmail.com>
2019-12-25 10:29:54 -0500

    Removed unneeded comments.

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,8 +1,6 @@
 <div class="footer">
     <p class="footer-text">&copy;MoviesWatchedApp - This website is purely for educational purposes.</p>
 </div>
-<script>
-    console.log('test');
-</script>
+
 </body>
 </html>
